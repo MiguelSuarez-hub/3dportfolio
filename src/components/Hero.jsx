a93ef821df458853acd6dc1cad8bf52c6845ce50
Mike <miguel.suarezf05@gmail.com>
2023-05-08 10:58:17 -0500

    Finished the hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,6 +20,23 @@ const Hero = () => {
         </div>
       </div>
       <ComputersCanvas />
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start">
+            <motion.dev 
+              animate={{
+                y: [1, 45, 1]
+              }}
+              transition={{
+                duration: 1.3,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
